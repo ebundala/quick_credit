6f37a887c594d96f68b5f33cc92d7689b0993764
ebundala <ebundala@gmail.com>
2019-04-29 20:18:28 +0300

    added git ignore

--- UI/assets/js/app.js
+++ UI/assets/js/app.js
@@ -1,2 +1,17 @@
+/* 
+*@description app entry point
+*@param e DOMContentLoaded event 
+*/
+function main(e) {
+    console.log("hello world");
+}
+/*
+*@description utility function to query the dom
+*@param selector a css selector to query
+*@return a dom node or null
+*/
+function $(selector) {
+    return document.querySelector(selector)
+}
 
-console.log("hello world");
+document.addEventListener("DOMContentLoaded", main)
