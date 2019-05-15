c91d2136d866b194367566816ca0f292f886f147
ebundala <ebundala@gmail.com>
2019-05-15 16:41:05 +0300

    chore(openapi url):fixed

--- server/config/config.js
+++ server/config/config.js
@@ -2,4 +2,4 @@
  * Created by ebundala on 5/15/2019.
  */
 
-export const baseUrl="http://localhost/api/v1";
+export const baseUrl="https://peaceful-stream-51240.herokuapp.com/api/v1/api-docs/"; //"http://localhost/api/v1";
