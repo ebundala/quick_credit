2cf5e26c958c11778cdd5bdf907f4a844adc4a69
ebundala <ebundala@gmail.com>
2019-05-17 06:13:13 +0300

    feature(admin can can view a specific loan):complete

--- server/controllers/GetSpecificLoan.js
+++ server/controllers/GetSpecificLoan.js
@@ -1,5 +1,24 @@
-const getSpecificLoan=(req,res,next)=>{
-    res.send("OK")
+import User from "../models/User"
+import ErrorResponse from "../models/ErrorResponse"
+import SuccessResponse from "../models/SuccessResponse"
+
+const getSpecificLoan=(req,res)=>{
+    let userModel=new User();
+    let loanId=req.params.loanId;
+    if(loanId){
+        let loan=userModel.getLoanById(loanId);
+        if(loan){
+            res.status(200)
+            res.json(new SuccessResponse(loan))
+        }else{
+            res.status(404)
+            res.json(new ErrorResponse("not found",404))
+        }
+    }
+    else{
+        res.status(400)
+        res.json(new ErrorResponse("invalid input",400))
+    }
 }
 
 export default getSpecificLoan;
