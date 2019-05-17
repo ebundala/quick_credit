6cfa6d093c61b2aeb1d32d3df7cbce851dd0b0f3
ebundala <ebundala@gmail.com>
2019-05-17 06:04:59 +0300

    feature(admin can verify clients):complete

--- server/controllers/VerifyUser.js
+++ server/controllers/VerifyUser.js
@@ -1,6 +1,27 @@
+import User from "../models/User"
+import ErrorResponse from "../models/ErrorResponse"
+import SuccessResponse from "../models/SuccessResponse"
 
-const verifyUser=(req,res,next)=>{
-    res.send("OK")
+const verifyUser=(req,res)=>{
+  let userModel=  new User();
+  let email=req.params.email;
+  let token = req.token;
+  if(email&&(token&&token.isAdmin)){
+    let user=userModel.getUserByEmail(email);
+      if(user&&(user.status===userModel.UNVERIFIED)){
+          user.status=userModel.VERIFIED;
+          let aprovedUser=userModel.patchUser(user);
+          res.status(200);
+          res.json(new SuccessResponse(aprovedUser));
+      }else{
+          res.status(400);
+          res.json(new ErrorResponse("user already verified or doesn't exist"));
+      }
+  }else{
+    res.status(400);
+      res.json(new ErrorResponse("invalid email"));
+
+  }
 }
 
 export default verifyUser;
