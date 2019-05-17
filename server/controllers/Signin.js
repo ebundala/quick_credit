ddec043525d0a7d53b81090f0a31373f040432fa
ebundala <ebundala@gmail.com>
2019-05-17 05:58:32 +0300

    feature(user can sign in):complete

--- server/controllers/Signin.js
+++ server/controllers/Signin.js
@@ -1,5 +1,23 @@
-const signin=(req,res,next)=>{
-    res.send("OK")
+import User from "../models/User"
+import ErrorResponse from "../models/ErrorResponse"
+//import SuccessResponse from "../models/SuccessResponse"
+
+const signin=(req,res)=>{
+    let userModel = new User();
+   let credentials=req.body;
+    if(credentials&&credentials.email&&credentials.password){
+        let userResp=userModel.signin(credentials.email,credentials.password);
+        if (userResp&&userResp.code===200){
+            res.status(200);
+            res.json(userResp);
+        }else{
+            res.status(400);
+            res.json(new ErrorResponse("invalid credentials",400));
+        }
+    }else {
+        res.status(400);
+        res.json(new ErrorResponse("invalid credentials",400));
+    }
 }
 
 export default signin;
